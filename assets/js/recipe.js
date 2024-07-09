7ec68fcd025f075639ed1c732a9423f65bca2b57
kc-claas <keithclaas@gmail.com>
2024-07-09 18:22:13 -0500

    started edit function

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -8,15 +8,21 @@ const ingredients = document.querySelector(`#ingredients-recipe`)
 const instructions = document.querySelector(`#instructions-recipe`)
 const title = document.querySelector(`title`)
 
+const edit = document.querySelector('#edit-recipe')
+const nameEdit = document.querySelector('#name-edit')
+const cookTimeEdit = document.querySelector('cook-time-edit')
+const ingredientsEdit = document.querySelector('ingredients-edit')
+const instructionsEdit = document.querySelector('instructions-edit')
+
+const index = localStorage.getItem(`recipe-choice`)
 
 const renderRecipe = function(array) {
     if (localStorage.getItem(`recipe-choice`)) {
-    const index = localStorage.getItem(`recipe-choice`)
-    title.textContent = `${array[index].name}`
-    recipeName.textContent = `${array[index].name}`
-    cookTime.textContent = `${array[index].cooktime}`
-    ingredients.textContent = `${array[index].ingredients}`
-    instructions.textContent = `${array[index].instructions}`
+    title.textContent = `${array[index].name}`;
+    recipeName.textContent = `${array[index].name}`;
+    cookTime.textContent = `${array[index].cooktime}`;
+    ingredients.textContent = `${array[index].ingredients}`;
+    instructions.textContent = `${array[index].instructions}`;
     }
     else {
     recipeName.textContent = `Recipe not found`
@@ -27,6 +33,17 @@ const renderRecipe = function(array) {
 
 
 renderRecipe(readRecipeList())
+
+// to-do add functionality to edit current recipe
+
+edit.addEventListener('click', function() {
+nameEdit.innerText(`${array[`recipe-choice`].name}`)
+cookTimeEdit.innerText(`${array[`recipe-choice`].cooktime}`)
+ingredientsEdit.innerText(`${array[`recipe-choice`].ingredients}`)
+instructionsEdit.innerText(`${array[`recipe-choice`].instructions}`)
+})
+
+
 
 
 // to-do addEventListener for back button to redirect to landing page
