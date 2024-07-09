04b630f5a9bbf7a97abe06b0eb3da0c7eabf617f
ZapApriendo <isaiah.capers@outlook.com>
2024-07-08 21:19:00 -0400

    moved js

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,30 +1,4 @@
-const nameInput = document.querySelector('#name-form')
-const cookInput = document.querySelector('#cook-time-form')
-const ingredientInput = document.querySelector('#ingredients-form')
-const instructionInput = document.querySelector('#instructions-form')
 
-function displayMessage(type, message) {
-    error.textContent = message;
-  }
-
-function addRecipe(event) {
-    event.preventDefault();
-
-    const recipeList = {
-        cooktime: cookInput.value.trim(),
-        ingredients: ingredientInput.value.trim(),
-        instructions: instructionInput.value.trim()
-    };
-    if (recipeList.cooktime === '' || recipeList.ingredients === '' || recipeList.instructions === '') {
-        displayMessage('error', "Please complete the form.");
-      } else {
-        displayMessage('success', 'Submitted Recipe Successfully');
-        recipeStorage(recipeList)
-        
-        let redirectURL = "./recipe.html";
-        redirectPage (redirectURL);
-      }
-}
 
 
 form.addEventListener('submit',addRecipe);
